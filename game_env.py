b514dfb3f7877547526e2f18da72aaeca88204eb
heweikang <heweikang@yafex.cn>
2024-03-31 20:34:56 +0800

    adjust

--- game_env.py
+++ game_env.py
@@ -103,7 +103,7 @@ class EmulatorEnv:
         self.asset = current_market_value + self.available_cash
         
         reward = np.sum(self.tomorrow_prices() * self.volumes) - current_market_value
-        reward = reward * 0.001
+        reward = np.log(reward - 1)  # 对数曲线，防止训练中出现超大值，破坏稳定
         # Check if the end of the dataset is reached
         self.current_day += 1
         done = self.current_day > self.end_day - 2# or self.asset < 10
